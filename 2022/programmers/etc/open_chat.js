96cf9b378a05d82042c099542102b4fff384f26d
suu3 <ahob789@ewhain.net>
2022-04-27 01:35:30 +0900

    [programmers] 오픈채팅방

---
+++ 2022/programmers/etc/open_chat.js
@@ -0,0 +1,25 @@
+// 2019 KAKAO BLIND RECRUITMENT
+
+function solution(record) {
+  let answer = [];
+  let map = new Map();
+  for (let i = 0; i < record.length; i++) {
+    const [action, id, name] = record[i].split(" ");
+
+    if (action === "Enter") {
+      map.set(id, name);
+      answer.push(id + "님이 " + "들어왔습니다.");
+    } else if (action === "Leave") {
+      answer.push(id + "님이 " + "나갔습니다.");
+    } else {
+      map.set(id, name);
+      continue;
+    }
+  }
+
+  answer = answer.map((str) => {
+    const tempArray = str.split("님이 ");
+    return map.get(tempArray[0]) + "님이 " + tempArray[1];
+  });
+  return answer;
+}
